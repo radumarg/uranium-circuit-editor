be97c7859a01f7a546628f5afd67afaefb1acc49
Radu Marginean <radu.marg@gmail.com>
2021-07-07 14:55:38 +0300

    More changes towards duplicating gates.

--- circuit-editor/src/components/ParametricGateSwap.vue
+++ circuit-editor/src/components/ParametricGateSwap.vue
@@ -3,7 +3,7 @@
 
     <img :src="gateImageSrcEditor" :title="title" data-toggle="tooltip" :name="name" @dragend="dragEnd" @dragstart="dragStart" style="width:100%;height:100%;max-width:40px;max-height:40px;min-width:40px;min-height:40px;"/>
     
-    <b-modal ref="modal-dialog" size="sm" centered hide-footer hide-header>
+    <b-modal ref="initial-modal-dialog" size="sm" centered hide-footer hide-header>
 
       <table style="table-layout:fixed;">
         <tr>
@@ -68,7 +68,12 @@
           <td></td>
         </tr>
         <tr>
-          <td></td>
+          <td class="no-resize-cell">
+             <div v-b-hover="handleExpandGateHover">
+              <b-icon v-if="expandGateIsHovered" v-on:click="handleExpandGate()" icon="files" v-b-tooltip.hover title="Expand gate" style="color: #7952b3;" font-scale="1.5"></b-icon>
+              <b-icon v-else icon="files" v-on:click="handleExpandGate()" style="color: #7952b3;" font-scale="1.3"></b-icon>
+              </div>
+          </td>
           <td colspan="2">
             <div v-b-hover="handleExpandDownHover">
               <b-icon v-if="expandDownIsHovered" icon="chevron-bar-down" v-on:click="expandCircuitDown()" v-b-tooltip.hover title="Add qbit after" style="color: #7952b3;" font-scale="1.6"></b-icon>
@@ -85,6 +90,83 @@
       </table>
 
     </b-modal>
+
+    <b-modal ref="replicate-gate-modal-dialog" size="lg" width="100px" centered hide-footer hide-header>
+      <table>
+        <tr>
+          <td colspan="6">
+          </td>
+          <td class="no-resize-cell">
+            <div v-b-hover="handleSecondModalCloseHover">
+              <b-icon v-if="secondModalCloseIsHovered" v-on:click="hideSecondModal()" icon="x-square" style="color: #7952b3;" font-scale="1.6"></b-icon>
+              <b-icon v-else icon="x-square" v-on:click="hideSecondModal()" style="color: #7952b3;" font-scale="1.4"></b-icon>
+            </div>
+          </td>
+        </tr>
+        <tr>
+          <td width="100px" class="td-2nd-modal">
+            First Qubit:
+          </td>
+          <td width="100px" class="td-2nd-modal">
+            <b-form-input min="0" v-model="qbitStart" placeholder="q" type="number" id="qbit-start" style="width:75px;"></b-form-input>
+          </td>
+          <td width="100px" class="td-2nd-modal">
+            Last Qubit:
+          </td>
+          <td width="100px" class="td-2nd-modal">
+            <b-form-input min="0" v-model="qbitStop" placeholder="q" type="number" id="qbit-stop" style="width:75px;"></b-form-input>
+          </td>
+          <td width="200px" class="td-2nd-modal">
+            Condition - 'q' based <br/>javascript expression:
+          </td>
+          <td width="100px" class="td-2nd-modal">
+            <b-form-input min="0" v-model="qbitConditionExpression" placeholder="q > 0" type="text" id="qbit-cond" style="width:120px;"></b-form-input>
+          </td>
+          <td class="no-resize-cell"></td>
+        </tr>
+        <tr>
+          <td width="100px" class="td-2nd-modal">
+            First Step:
+          </td>
+          <td width="100px" class="td-2nd-modal">
+            <b-form-input min="0" v-model="stepStart" placeholder="s" type="number" id="step-start" style="width:75px;"></b-form-input>
+          </td>
+          <td width="100px" class="td-2nd-modal">
+            Last Step:
+          </td>
+          <td width="100px" class="td-2nd-modal">
+            <b-form-input min="0" v-model="stepStop" placeholder="s" type="number" id="step-stop" style="width:75px;"></b-form-input>
+          </td>
+          <td width="200px" class="td-2nd-modal">
+            Condition - 's' based <br/>javascript expression:
+          </td>
+          <td width="100px" class="td-2nd-modal">
+            <b-form-input min="0" v-model="stepConditionExpression" placeholder="s > 0" type="text" id="step-cond" style="width:120px;"></b-form-input>
+          </td>
+          <td class="no-resize-cell"></td>
+        </tr>
+        <tr>
+          <td colspan="3" width="300px" class="td-2nd-modal">
+            Conjugate Condition - 'q, s' based <br/>javascript expression:
+          </td>
+          <td colspan="3" width="400px" class="td-2nd-modal">
+            <b-form-input min="0" v-model="conjugateConditionExpression" placeholder="" type="text" id="conjugate-cond" style="width:300px;"></b-form-input>
+          </td>
+          <td class="no-resize-cell"></td>
+        </tr>
+        <tr>
+          <td colspan="6" class="td-2nd-modal">
+          </td>
+          <td class="no-resize-cell">
+            <div v-b-hover="handleSecondModalSaveHover">
+              <b-icon v-if="secondModalSaveIsHovered" v-on:click="handleSecondModalSave()" icon="check" style="color: #7952b3;" font-scale="1.8"></b-icon>
+              <b-icon v-else icon="check" v-on:click="handleSecondModalSave()" style="color: #7952b3;" font-scale="1.4"></b-icon>
+            </div>
+          </td>
+        </tr>
+      </table>
+    </b-modal>
+
 
   </div>
 </template>
@@ -141,6 +223,30 @@ export default {
       );
       this.$refs['modal-dialog'].hide();
     },
+    handleSecondModalSave: function(){
+      let qbitOld = this.qbit;
+      let promise = this.duplicateGate({
+        'step': this.step,
+        'qbit': this.qbit,
+        'name': this.name, 
+        'stepStart': this.stepStart,
+        'stepStop': this.stepStop,
+        'stepConditionExpression': this.stepConditionExpression,
+        'qbitStart': this.qbitStart,
+        'qbitStop': this.qbitStop,
+        'qbitConditionExpression': this.qbitConditionExpression,
+        'conjugateConditionExpression': this.conjugateConditionExpression,
+      });
+      promise.then(
+        // eslint-disable-next-line no-unused-vars
+        result => {}, 
+        // eslint-disable-next-line no-unused-vars
+        error => {
+          this.$data.qbitNew = this.qbit = qbitOld;
+        }
+      );
+      this.$refs['replicate-gate-modal-dialog'].hide();
+    },
     dragStart: function(event) {
       hideTooltips();
       const target = event.target;
@@ -171,13 +277,15 @@ th,
 td {
   padding: 1px;
 }
+.td-2nd-modal {
+  padding: 5px;
+}
 .no-resize-cell{
   width: 35px;
   max-width: 35px;
   height: 35px;
   max-height: 35px;
 }
-
 img {
   display: inline-block;
 }
